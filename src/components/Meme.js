7600f86f779e2847c4424571053669432d7917ee
Katiasun <katjatolok3@gmail.com>
2024-03-27 22:40:10 +0100

    delete laabelas in Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,10 +5,7 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <label htmlFor="top-text">Top Text</label>
-        <input type="text" id="top-text" placeholder="Top text" className="form-input" />
-
-        <label>Bottom Text</label>
+        <input type="text" placeholder="Top text" className="form-input" />
         <input type="text" id="" placeholder="Bottom text" className="form-input" />
 
         <button className="form-button">Get a new meme image</button>
